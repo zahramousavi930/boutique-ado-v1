643c6392379f1b24d4d33717abeabe50f13ab3b9
Zahra <zahramousavi930@gmail.com>
2023-06-06 10:18:53 +0100

    customized admin

--- home/urls.py
+++ home/urls.py
@@ -5,3 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home')
 ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Other URL patterns
+    path('profile/', views.profile_view, name='profile'),
+]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,8 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+from django.shortcuts import render
+
+def profile_view(request):
+    # Your view logic here
+    return render(request, 'profile.html')
