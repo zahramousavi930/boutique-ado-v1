2d071a3d7d6d38be10a5f4a1ab36394b344af352
Zahra <zahramousavi930@gmail.com>
2023-06-09 22:32:47 +0100

    added shopping bag app, urls and templets

--- home/views.py
+++ home/views.py
@@ -6,8 +6,7 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
-from django.shortcuts import render
 
 def profile_view(request):
-    # Your view logic here
-    return render(request, 'profile.html')
+    # Your view logic goes here
+    pass
