e804f90e08dc2ad2ec6f81ca1c0fb041e5ba249e
Zahra <zahramousavi930@gmail.com>
2023-06-11 20:10:45 +0100

    added loading overlay

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -34,10 +34,10 @@ card.addEventListener('change', function (event) {
 	if (event.error) {
 		var html = `
             <span class="icon" role="alert">
-            <i class="fas fa-times"></i>
+                <i class="fas fa-times"></i>
             </span>
             <span>${event.error.message}</span>
-           `;
+        `;
 		$(errorDiv).html(html);
 	} else {
 		errorDiv.textContent = '';
@@ -51,6 +51,8 @@ form.addEventListener('submit', function (ev) {
 	ev.preventDefault();
 	card.update({ disabled: true });
 	$('#submit-button').attr('disabled', true);
+	$('#payment-form').fadeToggle(100);
+	$('#loading-overlay').fadeToggle(100);
 	stripe
 		.confirmCardPayment(clientSecret, {
 			payment_method: {
@@ -66,6 +68,8 @@ form.addEventListener('submit', function (ev) {
                 </span>
                 <span>${result.error.message}</span>`;
 				$(errorDiv).html(html);
+				$('#payment-form').fadeToggle(100);
+				$('#loading-overlay').fadeToggle(100);
 				card.update({ disabled: false });
 				$('#submit-button').attr('disabled', false);
 			} else {
